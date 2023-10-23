48a56d7638355f497dacc848f97338ac352d7994
Marius Mircioaga <bbla04936@gmail.com>
2023-10-23 21:47:04 +0300

    ox test

--- api/index.py
+++ api/index.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+import osmnx as ox
+import networkx as nx
+import fastapi
 
 app = FastAPI()
 
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+    
+    
+    
+@app.get("/nodes")
+def read_nodes():
+    point = (40.740, -73.990)
+    distance = 1000
+    G = ox.graph_from_point(point, dist=distance, network_type='drive')
+
+    return {"nodes": len(list(G.nodes()))}
+
+    
+    
+    
